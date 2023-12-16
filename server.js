e7fc587811c7706ca5652a97ee0b8bdc1f021b88
Arthur Magnette <amagnette@magicsoftware.com>
2023-12-16 15:28:34 +0100

    Options done

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/connexion', (req, res) => {
         res.status(500).json({ error: 'Erreur interne du serveur' });
       } else if (row) {
         // Créez un jeton JWT
-        const token = jwt.sign({ email }, 'votre_clé_secrète', { expiresIn: '1h' });
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1h' });
         // Utilisateur trouvé, connexion réussie
         res.status(200).json({ success: true, token });
       } else {
@@ -65,8 +65,74 @@ app.post('/connexion', (req, res) => {
   });
 
 
+// Middleware pour vérifier le jeton JWT
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization'].trim();
+  const token_clear = token.split(' ')[1];
+  if (!token) {
+    return res.status(403).json({ error: 'Token missing' });
+  }
+
+  console.log('Received Token:', token_clear);
+  jwt.verify(token_clear, 'secret', { algorithms: ['HS256'] }, (err, decoded) => {
+    if (err) {
+      console.error('Error decoding token:', err);
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({ error: 'Token expired' });
+      } else {
+        return res.status(401).json({ error: 'Token authentication failed' });
+      }
+    }
+  
+    console.log('Decoded token:', decoded);
+    req.user = decoded;
+    next();
+  });  
+}
 
 
+
+// Route pour récupérer les données de l'utilisateur connecté
+app.get('/user', verifyToken, (req, res) => {
+  const { email } = req.user;
+
+  const sql = 'SELECT nom, prenom, adresse, code_postal, ville, telephone, email, mdp FROM utilisateurs WHERE email = ?';
+  const values = [email];
+
+  db.get(sql, values, (err, row) => {
+    if (err) {
+      console.error('Erreur lors de la récupération des données de l\'utilisateur :', err);
+      res.status(500).json({ error: 'Erreur interne du serveur' });
+    } else if (row) {
+      res.status(200).json(row);
+    } else {
+      res.status(404).json({ error: 'Utilisateur non trouvé' });
+    }
+  });
+});
+
+// Update user data endpoint
+app.put('/user', verifyToken, (req, res) => {
+  const { email } = req.user;
+  const { nom, prenom, adresse, codePostal, ville, telephone, mdp } = req.body;
+
+  const sql = `
+    UPDATE utilisateurs
+    SET nom = ?, prenom = ?, adresse = ?, code_postal = ?, ville = ?, telephone = ?, mdp = ?
+    WHERE email = ?
+  `;
+  const values = [nom, prenom, adresse, codePostal, ville, telephone, mdp, email];
+
+  // Using a prepared statement
+  db.run(sql, values, (err) => {
+    if (err) {
+      console.error('Erreur lors de la modification des données de l\'utilisateur :', err);
+      res.status(500).json({ error: 'Erreur interne du serveur' });
+    } else {
+      res.status(200).json({ success: true });
+    }
+  });
+});
 
 
 // Démarrer le serveur
