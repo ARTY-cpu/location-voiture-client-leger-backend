f9354a836eb8d64a5e886dfb2e026da42430d29b
Arthur Magnette <amagnette@magicsoftware.com>
2023-12-17 17:44:58 +0100

    desactiver compte

--- server.js
+++ server.js
@@ -76,27 +76,36 @@ app.post('/inscription', (req, res) => {
 
 // Route pour la connexion
 app.post('/connexion', (req, res) => {
-    const { email, mdp } = req.body;
-  
-    // Utiliser une requête préparée pour vérifier les informations de connexion dans la base de données
-    const sql = 'SELECT * FROM utilisateurs WHERE email = ? AND mdp = ?';
-    const values = [email, mdp];
-  
-    db.get(sql, values, (err, row) => {
-      if (err) {
-        console.error('Erreur lors de la vérification des informations de connexion :', err);
-        res.status(500).json({ error: 'Erreur interne du serveur' });
-      } else if (row) {
-        // Créez un jeton JWT
-        const token = jwt.sign({ email }, 'secret', { expiresIn: '1h' });
-        // Utilisateur trouvé, connexion réussie
-        res.status(200).json({ success: true, token });
-      } else {
-        // Aucun utilisateur trouvé avec les informations fournies
-        res.status(401).json({ error: 'Informations de connexion incorrectes' });
+  const { email, mdp } = req.body;
+
+  // Utiliser une requête préparée pour vérifier les informations de connexion dans la base de données
+  const sql = 'SELECT * FROM utilisateurs WHERE email = ? AND mdp = ?';
+  const values = [email, mdp];
+
+  db.get(sql, values, (err, row) => {
+    if (err) {
+      console.error('Erreur lors de la vérification des informations de connexion :', err);
+      return res.status(500).json({ error: 'Erreur interne du serveur' });
+    }
+
+    if (row) {
+      // Vérifier si le compte est désactivé
+      if (row.Desactive === 1) {
+        // Compte désactivé, renvoyer une erreur
+        return res.status(401).json({ error: 'Votre compte est désactivé. Veuillez contacter l\'administrateur.' });
       }
-    });
+
+      // Compte actif, créer un jeton JWT
+      const token = jwt.sign({ email }, 'secret', { expiresIn: '1h' });
+      // Utilisateur trouvé, connexion réussie
+      return res.status(200).json({ success: true, token });
+    } else {
+      // Aucun utilisateur trouvé avec les informations fournies
+      return res.status(401).json({ error: 'Informations de connexion incorrectes' });
+    }
   });
+});
+
 
 
 // Middleware pour vérifier le jeton JWT
@@ -167,6 +176,28 @@ app.put('/user', verifyToken, (req, res) => {
     }
   });
 });
+
+
+
+// route desactivation compte
+app.post('/desactiver-compte', verifyToken, (req, res) => {
+  const { email } = req.user;
+  const { mdp } = req.body;
+
+  // Logique pour vérifier le mot de passe et désactiver le compte
+  const sql = 'UPDATE utilisateurs SET desactive = 1 WHERE email = ? AND mdp = ?';
+  const values = [email, mdp];
+
+  db.run(sql, values, (err) => {
+    if (err) {
+      console.error('Erreur lors de la désactivation du compte :', err);
+      res.status(500).json({ error: 'Erreur interne du serveur' });
+    } else {
+      res.status(200).json({ success: true });
+    }
+  });
+});
+
 
 
 // Démarrer le serveur
